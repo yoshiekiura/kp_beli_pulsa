cf72b2bad011eceab0814339821a397a786f492c
rendycahyae <rendycahya6@qmail.id>
2020-03-12 14:48:55 +0700

    Tambah Seeder

---
+++ beli_pulsa/storage/framework/views/469ccd87b2e76e1c8778efb784e7e8fc776457ed.php
@@ -0,0 +1,277 @@
+<?php $__env->startSection('title','Beli Pulsa Murah Disini Aja :D'); ?>
+
+<?php $__env->startSection('body'); ?>
+
+<div id="pageone">
+    <section class="section">
+          <div class="container mt-5 animated bounceInDown delay-0s">
+            <?php if(count($errors) > 0): ?>
+            <div class="alert alert-danger alarm mt-3 animated jackInTheBox delay-2s">
+                    <h3 class="text-center">Kesalahan Pengisian</h3>
+                    <ul>
+                        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <li><?php echo e($error); ?></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                    </div>
+            <?php endif; ?>
+
+            
+
+            <div class="row no-gutters bg-light position-relative rounded-left rounded-right turuninDikit shadow-lg p-3 mb-4 bg-white rounded">
+                <div class="col-md-6 mb-md-0 p-md-4">
+                  <img src="/image/image1.jpg" class="w-100" alt="picture">
+                </div>
+                <div class="col-md-6 position-static p-4 pl-md-0">
+                    <div class="text-dark text-center">
+                        <h6 class="font-weight-bold animated bounceInDown delay-1s mt-5 mb-3">Mau pulsa murah tapi nggak ribet ?</h6>
+                        <h3 class="font-weight-bold animated zoomIn delay-1s">PESAN DISINI AJA</h3>
+                        <h5 class="font-weight-bold animated zoomIn delay-1s">TUKUPULSA.COM</h5>
+                        <h6 class="font-weight-bold animated bounceInUp delay-1s mt-3 mb-5">Ada untuk Anda.</h6>
+                        <a type="button" class="btn btn-info animated jackInTheBox delay-2s mt-5" id="beli" href="#pilih-paket">BELI SEKARANG</a>
+                    </div>
+                </div>
+              </div>
+          </div>
+
+          <div class="row text-center animated bounceInUp delay-3s">
+              <div class="col-lg">
+                  <p class="font-weight-bold">DAFTAR UNTUK JADI ANGGOTA TUKUPULSA.COM</p>
+                  <a href="/Daftar" class="btn btn-warning font-weight-bold mr-3">DAFTAR SEKARANG <i class="fas fa-user-plus"></i></a>
+                  <a href="/Login" class="btn btn-success font-weight-bold">LOGIN <i class="fas fa-sign-in-alt"></i></a>
+              </div>
+          </div>
+        </section>
+        </div>
+
+        <div class="separator">
+            <h3>Pilih Paket</h3>
+        </div>
+
+<div id="pilih-paket">
+    <section class="section">
+        <div class="container container-responsive turuninDikit">
+            <div class="row  animated fadeInUp delay-0s">
+                <div class="col mb-3">
+                    <div class="card shadow-lg border-1 rounded-lg">
+                        <form action="/beli" method="post">
+                        <h2 class="text-center mt-4 mb-4"><span class="badge badge-pill badge-dark">PULSA REGULER</span></h2>
+
+                        <div class="form-group form-group-ukuran">
+                            <select name="pulsa_op" id="pulsa_op" class="browser-default custom-select dynamic1" data-dependent="pulsa_nominal" required>
+                                <option selected disabled value="">-- Pilih Operator --</option>
+                                <?php $__currentLoopData = $produk_pulsa; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $p): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <option value="<?php echo e($p->pulsa_op); ?>"><?php echo e($p->pulsa_op); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </select>
+                        </div>
+                    <div class="form-group form-group-ukuran">
+                        <select name="pulsa_nominal" id="keterangan1" class="browser-default custom-select opsi-pulsa" required>
+                            <option value="" selected>-- Pilih Nama Produk --</option>
+                        </select>
+                    </div>
+                    <?php echo csrf_field(); ?>
+                    <div class="form-group form-group-ukuran">
+                        <input type="text" class="form-control" id="masaaktif1" placeholder="Masa Aktif" disabled>
+                    </div>
+                    <?php if(Session::get('nama')): ?>
+                    <div class="form-group form-group-ukuran">
+                    <input type="number" name="nomor" class="form-control" placeholder="Nomor HP" value="<?php echo e($nomor[0]->no_telpon); ?>" readonly required>
+                    </div>
+                    <?php else: ?>
+                        <div class="form-group form-group-ukuran">
+                            <input type="number" name="nomor" class="form-control" placeholder="-- Nomor HP --" required>
+                        </div>
+                    <?php endif; ?>
+                    <div class="form-group form-group-ukuran">
+                        <select name="bank" class="browser-default custom-select" required>
+                            <option selected disabled>-- Pilih Pembayaran --</option>
+                            <?php $__currentLoopData = $bank; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $b): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <option value="<?php echo e($b->id); ?>"><?php echo e($b->bank); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </select>
+                    </div>
+                    <div class="form-group form-group-ukuran">
+                        <h2 class="harga hr1">RP -</h2>
+                        <input type="hidden" name="harga" id="hr1" readonly>
+                        <input type="hidden" name="kode"  id="kode1" readonly>
+
+                    </div>
+                    <div class="form-group form-group-ukuran">
+                        <button type="submit" class="btn btn-outline-primary btn-pesan">PESAN SEKARANG</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+
+                <div class="col">
+                    <div class="card shadow-lg border-1 rounded-lg">
+                        <h2 class="text-center mt-4 mb-4"><span class="badge badge-pill badge-dark">PAKET INTERNET</span></h2>
+                        <form action="/beli" method="post">
+                            <div class="form-group form-group-ukuran">
+                                <select name="pulsa_op" id="pulsa_op" class="browser-default custom-select dynamic2" data-dependent="pulsa_nominal" required>
+                                    <option value="">-- Pilih Operator --</option>
+                                    <?php $__currentLoopData = $produk_internet; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $p): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <option value="<?php echo e($p->pulsa_op); ?>"><?php echo e($p->pulsa_op); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </select>
+                            </div>
+                            <div class="form-group form-group-ukuran">
+                                <select name="pulsa_nominal" id="keterangan2" class="browser-default custom-select opsi-paket" required>
+                                <option value="" selected>-- Pilih Nama Produk --</option>
+                                </select>
+                            </div>
+                            <?php echo csrf_field(); ?>
+                            <div class="form-group form-group-ukuran">
+                                <input type="text" class="form-control" id="masaaktif2" placeholder="Masa Aktif" disabled>
+                            </div>
+                            <?php if(Session::get('nama')): ?>
+                                <div class="form-group form-group-ukuran">
+                                    <input type="number" name="nomor" class="form-control" placeholder="-- Nomor HP --" value="<?php echo e($nomor[0]->no_telpon); ?>" readonly required>
+                                </div>
+                            <?php else: ?>
+                                <div class="form-group form-group-ukuran">
+                                    <input type="number" name="nomor" class="form-control" placeholder="-- Nomor HP --" required>
+                                </div>
+                            <?php endif; ?>
+                            <div class="form-group form-group-ukuran">
+                                <select name="bank" class="browser-default custom-select" required>
+                                    <option selected disabled>-- Pilih Pembayaran --</option>
+                                    <?php $__currentLoopData = $bank; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $b): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <option value="<?php echo e($b->id); ?>"><?php echo e($b->bank); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </select>
+                            </div>
+                            <div class="form-group form-group-ukuran">
+                                <h2 class="harga hr2" name="harga">RP -</h2>
+
+                                <input type="hidden" name="harga" id="hr2" readonly>
+                                <input type="hidden" name="kode"  id="kode2" readonly>
+
+
+                            </div>
+                            <div class="form-group form-group-ukuran">
+                                <button type="submit" class="btn btn-outline-primary btn-pesan">PESAN SEKARANG</button>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </section>
+</div>
+
+
+<div class="backTop"><i class="fas fa-chevron-up"></i></div>
+
+<script>
+    $(document).ready(function(){
+
+        $('#beli').click(function(){
+            $('.card').addClass(' animated fadeInUp delay-1s');
+        });
+
+        $('.dynamic1').change(function(){
+            if($(this).val() != ''){
+                var select = $(this).attr("id");
+                var value = $(this).val();
+                var dependent = $(this).data('dependent');
+                var _token = $('input[name="_token"]').val();
+                $.ajax({
+                    url: '<?php echo e(url("dynamicdependent/cari")); ?>',
+                    method:"POST",
+                    data:{select:select, value:value, _token:_token, dependent:dependent},
+                    success:function(result){
+                        $('.opsi-pulsa').html(result);
+                    }
+                })
+            }
+        });
+
+        $('.dynamic2').change(function(){
+            if($(this).val() != ''){
+                var select = $(this).attr("id");
+                var value = $(this).val();
+                var dependent = $(this).data('dependent');
+                var _token = $('input[name="_token"]').val();
+                $.ajax({
+                    url: '<?php echo e(url("dynamicdependent/cari")); ?>',
+                    method:"POST",
+                    data:{select:select, value:value, _token:_token, dependent:dependent},
+                    success:function(result){
+                        $('.opsi-paket').html(result);
+                    }
+                })
+            }
+        });
+
+        $('#keterangan1').change(function(){
+            var provider = $('.dynamic1').val();
+            var ket = $(this).val();
+            var _token = $('input[name="_token"]').val();
+            //console.log(ket);
+
+            $.ajax({
+                url: '<?php echo e(url("dynamicdependent/bawaKodeHarga")); ?>',
+                method: 'POST',
+                data:{keterangan:ket, _token:_token},
+                dataType: "JSON",
+                success:function(data){
+                    //console.log(data);
+
+                    $('.hr1').html("RP. "+ data.pulsa_price);
+                    $('#hr1').val(data.pulsa_price);
+                    $('#masaaktif1').val("+ " +data.masaaktif + " Hari");
+                    $('#kode1').val(data.pulsa_code);
+                    // $('#produk1').val(data.pulsa_op);
+                    // $('#ket1').val(data.pulsa_nominal);
+                }
+            });
+        });
+
+        $('#keterangan2').change(function(){
+            var provider = $('.dynamic2').val();
+            var ket = $(this).val();
+            var _token = $('input[name="_token"]').val();
+            //console.log(ket);
+
+            $.ajax({
+                url: '<?php echo e(url("dynamicdependent/bawaKodeHarga")); ?>',
+                method: 'POST',
+                data:{keterangan:ket, _token:_token},
+                dataType: "JSON",
+                success:function(data){
+                    //console.log(data);
+
+                    $('.hr2').html("RP. "+ data.pulsa_price);
+                    $('#hr2').val(data.pulsa_price);
+                    $('#kode2').val(data.pulsa_code);
+                    $('#masaaktif2').val(data.masaaktif + " Hari");
+                    // $('#produk2').val(data.pulsa_op);
+                    // $('#ket2').val(data.pulsa_nominal);
+                }
+            });
+        });
+    });
+</script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript">
+        var $backToTop = $(".backTop");
+        $backToTop.hide();
+        $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            $backToTop.fadeIn();
+        } else {
+            $backToTop.fadeOut();
+        }
+        });
+
+        $backToTop.on('click', function(e) {
+        $("html, body").animate({scrollTop: 0}, 500);
+        });
+    </script>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout/main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\kp_beli_pulsa\beli_pulsa\resources\views//pelanggan/pages/beranda.blade.php ENDPATH**/ ?>
---
+++ beli_pulsa/storage/framework/views/8d2cc027b7d70fc0c16b2532e57e4c2b8fcafc8d.php
@@ -0,0 +1,78 @@
+<?php $__env->startSection('title','Admin TUKUPULSA'); ?>
+
+<?php $__env->startSection('body'); ?>
+
+    <?php if(!Session::get('admin')): ?>
+        <?php
+            Session::flash('CobaPaksa','Anda Harus Login Terlebih Dahulu!');
+            header("Location: " . URL::to('Login/Admin'), true, 302);
+            exit();
+        ?>
+    <?php endif; ?>
+
+    <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                    <h1 class="mt-4">Daftar Transaksi Tuku Pulsa</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="Home">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Daftar Transaksi</li>
+                        </ol>
+                        
+                    <div class="table-responsive">
+                      <form action="/mobilepulsa/pulsa" method="POST">
+                      <?php echo csrf_field(); ?>
+                          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                          <thead class="thead-dark">
+                            <tr>
+                              <th scope="col">No.</th>
+                              <th scope="col">Produk</th>
+                              <th scope="col">Provider</th>
+                              <th scope="col">Voucher</th>
+                              <th scope="col">No. Telepon</th>
+                              <th scope="col">Pembayaran</th>
+                              <th scope="col">Tanggal Pembelian</th>
+                              <th scope="col">Status Pembayaran</th>
+                              <th scope="col">Aksi</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                              <?php $__currentLoopData = $hasil; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <tr>
+                              <th scope="row"><?php echo e($loop->iteration); ?></th>
+                              <td>Paket <?php echo e($item->pulsa_type); ?></td>
+                              <input type="hidden" name="pulsa_code" value="<?php echo e($item->pulsa_code); ?>">
+                              <td><?php echo e($item->pulsa_op); ?></td>
+                              <td><?php echo e($item->pulsa_nominal); ?></td>
+                              <input type="hidden" name="no_telpon" value="<?php echo e($item->no_telpon); ?>">
+                              <td><?php echo e($item->no_telpon); ?></td>
+                              <td><?php echo e($item->bank); ?> ( <?php echo e($item->atas_nama); ?> )</td>
+                              <td><?php echo e($item->tanggal_beli); ?></td>
+                              <?php if($item->status_transaksi == 0): ?>
+                              <td>Menunggu</td>
+                              <td><input type="submit" value="Bayar"></td>
+                              
+                              <?php elseif($item->status_transaksi == 1): ?>
+                              <td>Proses</td>
+                              <td></td>
+                              
+                              <?php elseif($item->status_transaksi == 2): ?>
+                              <td>Selesai</td>
+                              <td></td>
+                              <?php endif; ?>
+                          </tr>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                          </tbody>
+                          </form>
+                        </table>
+
+                    </div>
+                    </div>
+                </main>
+
+    
+    
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\kp_beli_pulsa\beli_pulsa\resources\views/admin/pages/daftar_transaksi.blade.php ENDPATH**/ ?>
